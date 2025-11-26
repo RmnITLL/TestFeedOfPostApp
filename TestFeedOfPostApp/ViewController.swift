042e614444064255aa1c5b0ec2fa5284fd0c2c5f
Roman <inetmilo@rambler.ru>
2025-11-26 10:14:08 +0100

    main, created project

---
+++ TestFeedOfPostApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TestFeedOfPostApp
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
